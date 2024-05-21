fc6b5711db81afae1ee885726d46ac7a9aa0dd9e
Kachhadiya Dharmik <99231778+dharmik2003@users.noreply.github.com>
2024-05-21 11:13:13 +0530

    output:export

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['www.tasteofhome.com', 'res.cloudinary.com', 'cdn.svgator.com', 'i.pinimg.com', 'supershopping.lk','icon-library.com', 'c.tenor.com', 'github.com'],
     },
+    output: "export"
 };
 
 export default nextConfig;
