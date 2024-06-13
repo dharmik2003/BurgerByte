d5282c0961251726496935a141366d0bb49165d8
Kachhadiya Dharmik <99231778+dharmik2003@users.noreply.github.com>
2024-06-13 11:57:34 +0530

    spinner add

--- app/components/Myorder/OrderDetails.tsx
+++ app/components/Myorder/OrderDetails.tsx
@@ -148,7 +148,7 @@ const OrderDetails = () => {
                 ) : (
 
 
-                        <div className='w-full h-auto bg-[#f4f1ea] p-6 relative'>
+                        <div className='w-full h-[630px] bg-[#f4f1ea] p-6 relative'>
 
                             <h1 className='heading'>Order <span className='text-red-600'>Detail</span></h1>
                             <div className="lg:w-[70%] grid grid-cols-1 sm:grid-cols-2 gap-6 p-6">
