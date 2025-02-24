4103543d517c499bd63cb6360035572e4e10a147
Dharmik Kachhadiya <dharmikdk99@gmail.com>
2025-02-24 11:45:13 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['www.tasteofhome.com', 'res.cloudinary.com', 'cdn.svgator.com', 'i.pinimg.com', 'supershopping.lk','icon-library.com', 'c.tenor.com', 'github.com'],
-    },
+  output: "export",
+  images: {
+    domains: [
+      "www.tasteofhome.com",
+      "res.cloudinary.com",
+      "cdn.svgator.com",
+      "i.pinimg.com",
+      "supershopping.lk",
+      "icon-library.com",
+      "c.tenor.com",
+      "github.com",
+    ],
+  },
 };
 
 export default nextConfig;
